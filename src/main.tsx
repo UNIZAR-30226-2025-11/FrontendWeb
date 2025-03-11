9d08c5cef40520a5a37d49e59e73b56371e2c423
DavidTizne <davidtizne@gmail.com>
2025-03-11 12:49:00 +0100

    Add Socket Provider to the main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { SocketProvider } from './context/SocketContext';
 
 
 const rootElement = document.getElementById('root');
@@ -9,7 +10,9 @@ if (rootElement)
 {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </React.StrictMode>
   );
 } else {
