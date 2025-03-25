ccf8f2218ae5466c725d90d8273830ec21deaaf1
Davidjm123 <davidjmzom1@gmail.com>
2025-03-25 16:50:05 +0100

    env fixed

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,4 @@
 // Define defaults for the server
-export const SERVER_IP = import.meta.env.SERVER_IP || "http://localhost";
-export const SERVER_PORT = Number(import.meta.env.SERVER_PORT) || 8000;
-export const SERVER = SERVER_IP + ":" + String(SERVER_PORT)
-
-// Define defaults for the local server
-export const LOCAL_SERVER_IP = import.meta.env.LOCAL_SERVER_IP || "http://localhost"
-export const LOCAL_SERVER_PORT = Number(import.meta.env.LOCAL_SERVER_IP) || 5000;
-export const LOCAL_SERVER = LOCAL_SERVER_IP + ":" + String(LOCAL_SERVER_PORT)
+export const SERVER_IP = import.meta.env.VITE_SERVER_IP || "localhost";
+export const SERVER_PORT = Number(import.meta.env.VITE_SERVER_PORT) || 8000;
+export const SERVER = "http://" + SERVER_IP + ":" + String(SERVER_PORT)
