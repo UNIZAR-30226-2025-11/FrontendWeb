75fe50bf355f0e5d8dce5c27e9f614f7b7ca91b7
Davidjm123 <davidjmzom1@gmail.com>
2025-04-16 09:47:57 +0200

    Future cards done

--- src/components/game/FutureCards.tsx
+++ src/components/game/FutureCards.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BackendGamePlayedCardsResponseJSON, CardJSON } from "../../api/JSON";
 import Card from "./Card";
+import "./FutureCards.css";
 
 const FutureCards = (
     {
@@ -11,26 +12,50 @@ const FutureCards = (
     setCardPlayedResult:React.Dispatch<React.SetStateAction<BackendGamePlayedCardsResponseJSON | undefined>>
     }
 ) => {
-    return (
-        <div className="container-selection-big-user shadow-game">
-            <h1>The following cards are:</h1>
+    // Auto-close modal after 5 seconds
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCardPlayedResult(undefined);
+        }, 5000);
 
-            <div className="users">
-                {cards.map((card, key) => {
-                    return <Card    key={key}
+        return () => clearTimeout(timer);
+    }, [setCardPlayedResult]);
+
+    return (
+        <div className="future-cards-overlay" onClick={() => setCardPlayedResult(undefined)}>
+            <div className="future-cards-modal" onClick={(e) => e.stopPropagation()}>
+                <div className="future-cards-header">
+                    <h1>The Next 3 Cards</h1>
+                </div>
+                
+                <div className="future-cards-content">
+                    <div className="future-cards-sequence">
+                        {cards.map((card, index) => (
+                            <div className="future-card-container" key={index}>
+                                <div className="future-card-order">#{index + 1}</div>
+                                <Card 
+                                    key={index}
                                     card={card}
                                     isSelected={false}
                                     id={0}
                                     onClick={()=>{}}
-                                    setHoveredCard={()=>{}} />
-                })}
+                                    setHoveredCard={()=>{}} 
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="future-cards-info">
+                        <p>Cards shown from top to bottom of the deck</p>
+                        <p className="future-cards-tip">Click anywhere to close</p>
+                    </div>
+                </div>
+                
+                <div className="future-cards-timer">
+                    <div className="timer-bar"></div>
+                </div>
             </div>
-            
-            <button onClick={() => {setCardPlayedResult(undefined)}}>
-                Close
-            </button>
         </div>
-    )
-}
+    );
+};
 
 export default FutureCards;
