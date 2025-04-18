d982ebf3b73e81b3e2c45f3b63296fa38c02c3a2
Davidjm123 <davidjmzom1@gmail.com>
2025-04-18 15:43:32 +0200

    Invitation changed

--- src/common/InvitationModal/InvitationModal.tsx
+++ src/common/InvitationModal/InvitationModal.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSocket } from '../../context/SocketContext';
 import './InvitationModal.css';
+import { IMAGES_EXTENSION, IMAGES_PATH } from '../../services/apiShop';
 
 const InvitationModal: React.FC = () => {
   const { friendJoinRequest, acceptInvitation, declineInvitation } = useSocket();
+
+  // Auto-decline after 10 seconds
+  useEffect(() => {
+    if (!friendJoinRequest) return;
+    
+    const timer = setTimeout(() => {
+      if (friendJoinRequest) {
+        declineInvitation(friendJoinRequest.lobbyId);
+      }
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [friendJoinRequest, declineInvitation]);
 
   if (!friendJoinRequest) return null;
 
@@ -14,8 +28,15 @@ const InvitationModal: React.FC = () => {
           <h3>Lobby Invitation</h3>
         </div>
         <div className="invitation-modal-body">
+          {friendJoinRequest.friendSendingRequestAvatar && (
+            <img 
+              src={`${IMAGES_PATH}/avatar/${friendJoinRequest.friendSendingRequestAvatar}${IMAGES_EXTENSION}`} 
+              alt={`${friendJoinRequest.friendSendingRequest}'s avatar`} 
+              className="invitation-friend-avatar"
+            />
+          )}
           <p><strong>{friendJoinRequest.friendSendingRequest}</strong> has invited you to join their lobby</p>
-          <p className="lobby-details">Lobby ID: {friendJoinRequest.lobbyId}</p>
+          <div className="lobby-details">Lobby ID: {friendJoinRequest.lobbyId}</div>
         </div>
         <div className="invitation-modal-footer">
           <button 
@@ -31,6 +52,10 @@ const InvitationModal: React.FC = () => {
             Accept
           </button>
         </div>
+        <div className="invitation-timer">
+          <div className="invitation-timer-bar"></div>
+        </div>
+        <div className="invitation-timer-info">Auto-declining in 10 seconds</div>
       </div>
     </div>
   );
