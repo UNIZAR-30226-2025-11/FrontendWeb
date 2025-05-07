e2255c8451074bbec41a9e6923433ddf403f68e2
Davidjm123 <davidjmzom1@gmail.com>
2025-05-07 18:54:32 +0200

    http and https allowed

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,10 @@
 // Define defaults for the server
 const SERVER_IP = import.meta.env.VITE_SERVER_IP || "localhost";
+const SERVER_PORT = Number(import.meta.env.VITE_SERVER_PORT) || 8000;
 //export const SERVER_PORT = Number(import.meta.env.VITE_SERVER_PORT) || 8000;
 
-let server = SERVER_IP;
+const SERVER_PROTOCOL = import.meta.env.VITE_SERVER_PROTOCOL || "https";
 
-if (import.meta.env.VITE_SERVER_PORT) {
-  let SERVER_PORT: number = Number(import.meta.env.VITE_SERVER_PORT);
-  server += ":" + SERVER_PORT;
-}
-
-export const SERVER = server;
+export const SERVER =  SERVER_PROTOCOL + "://" + SERVER_IP + ":" + SERVER_PORT; // your server address
 export const PORT = Number(import.meta.env.VITE_PORT) || 5173; // your custom port
 
