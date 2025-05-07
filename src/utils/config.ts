45e9b30b3b109b75d78db0eabbfe3cd55f46081b
Davidjm123 <davidjmzom1@gmail.com>
2025-05-07 20:29:56 +0200

    config fixed

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,7 @@
 // Define defaults for the server
-const SERVER_IP = import.meta.env.VITE_SERVER_IP || "localhost";
+const SERVER_IP = import.meta.env.VITE_SERVER_IP || "http://localhost";
 const SERVER_PORT = Number(import.meta.env.VITE_SERVER_PORT) || 8000;
-//export const SERVER_PORT = Number(import.meta.env.VITE_SERVER_PORT) || 8000;
 
-const SERVER_PROTOCOL = import.meta.env.VITE_SERVER_PROTOCOL || "https";
-
-export const SERVER =  SERVER_PROTOCOL + "://" + SERVER_IP + ":" + SERVER_PORT; // your server address
+export const SERVER = SERVER_IP + ":" + SERVER_PORT; // your server address
 export const PORT = Number(import.meta.env.VITE_PORT) || 5173; // your custom port
 
